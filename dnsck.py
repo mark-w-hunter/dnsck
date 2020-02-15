88f7245559766c89f1b920836e2ae628a66bb4c2
Mark W. Hunter <marcus.w.hunter@gmail.com>
2020-02-15 13:59:21 -0500

    Add support for TCP DNS queries

--- dnsck.py
+++ dnsck.py
@@ -35,7 +35,7 @@ from itertools import groupby
 from dns import query, message, rcode, exception
 
 AUTHOR = "Mark W. Hunter"
-VERSION = "0.16"
+VERSION = "0.17"
 DEFAULT_RECORD_TYPE = "A"
 DEFAULT_ITERATIONS = 30
 
@@ -95,8 +95,62 @@ def dnsck_query_udp(dns_server, dns_query, record_type, iterations):
     print(f"Average response time: {sum(query_times) / len(query_times):.2f} ms\n")
 
 
+def dnsck_query_tcp(dns_server, dns_query, record_type, iterations):
+    """Perform a TCP DNS query for a set number of iterations"""
+    result_code_list = []
+    query_times = []
+    response_errors = 0
+    iteration_count = 0
+    make_dns_query = message.make_query(dns_query, record_type.upper())
+    print(
+        f"Performing {iterations} TCP queries to server {dns_server} for domain {dns_query}",
+        f"with record type {record_type.upper()}.\n"
+    )
+
+    try:
+        for iteration in range(iterations):
+            print(f"[Query {iteration + 1}]")
+            start_time = timeit.default_timer()
+            try:
+                dns_response = query.tcp(make_dns_query, dns_server, timeout=10)
+                if dns_response.answer:
+                    for answer in dns_response.answer:
+                        print(answer)
+                else:
+                    print("No records returned.")
+                result_code = rcode.to_text(dns_response.rcode())
+                result_code_list.append(result_code)
+                iteration_count += 1
+            except exception.Timeout:
+                print("Query timeout.")
+                result_code = "Timeout"
+                result_code_list.append(result_code)
+                iteration_count += 1
+                response_errors += 1
+            elapsed_time = (timeit.default_timer() - start_time) * 1000
+            time.sleep(1)
+            query_times.append(elapsed_time)
+            print(f"Response time: {round(elapsed_time, 2)} ms")
+            print(f"Response status: {result_code}\n")
+    except KeyboardInterrupt:
+        print("Program terminating...")
+
+    rcode_list_final = [(len(list(rcount)), rname) for rname, rcount in
+                        groupby(sorted(result_code_list))]
+
+    print("Response status breakdown:")
+    for count, query_rcode in rcode_list_final:
+        print(f"{count} {query_rcode}")
+    print(
+        f"\nSummary: Performed {iteration_count} TCP queries to server {dns_server}",
+        f"for domain {dns_query} with record type {record_type.upper()}.",
+        f"\nPacket loss: {response_errors / iteration_count * 100:.2f}%",
+    )
+    print(f"Average response time: {sum(query_times) / len(query_times):.2f} ms\n")
+
+
 if __name__ == "__main__":
-    if len(sys.argv) > 3:
+    if len(sys.argv) > 3 and "--tcp" not in sys.argv:
         if sys.argv[1] == "-s" and len(sys.argv) == 4:
             dnsck_query_udp(sys.argv[2], sys.argv[3], DEFAULT_RECORD_TYPE, DEFAULT_ITERATIONS)
         elif sys.argv[1] == "-s" and sys.argv[4] == "-t" and len(sys.argv) == 6:
@@ -109,6 +163,19 @@ if __name__ == "__main__":
             dnsck_query_udp(sys.argv[2], sys.argv[3], sys.argv[7], int(sys.argv[5]))
         else:
             print("Run dnsck.py -h for help.")
+    elif len(sys.argv) > 3 and "--tcp" in sys.argv:
+        if sys.argv[1] == "-s" and len(sys.argv) == 5:
+            dnsck_query_tcp(sys.argv[2], sys.argv[3], DEFAULT_RECORD_TYPE, DEFAULT_ITERATIONS)
+        elif sys.argv[1] == "-s" and sys.argv[4] == "-t" and len(sys.argv) == 7:
+            dnsck_query_tcp(sys.argv[2], sys.argv[3], sys.argv[5], DEFAULT_ITERATIONS)
+        elif sys.argv[1] == "-s" and sys.argv[4] == "-i" and len(sys.argv) == 7:
+            dnsck_query_tcp(sys.argv[2], sys.argv[3], DEFAULT_RECORD_TYPE, int(sys.argv[5]))
+        elif sys.argv[1] == "-s" and sys.argv[4] == "-t" and sys.argv[6] == "-i":
+            dnsck_query_tcp(sys.argv[2], sys.argv[3], sys.argv[5], int(sys.argv[7]))
+        elif sys.argv[1] == "-s" and sys.argv[4] == "-i" and sys.argv[6] == "-t":
+            dnsck_query_tcp(sys.argv[2], sys.argv[3], sys.argv[7], int(sys.argv[5]))
+        else:
+            print("Run dnsck.py -h for help.")
     elif len(sys.argv) == 1:
         print("Run dnsck.py -h for help.")
     elif sys.argv[1] == "--version" or sys.argv[1] == "-v":
@@ -116,7 +183,7 @@ if __name__ == "__main__":
     elif sys.argv[1] == "--help" or sys.argv[1] == "-h":
         print(
             "Usage: dnsck.py -s <server ip> <domain name> -t <record type>",
-            "-i <number of iterations>\n"
+            "-i <number of iterations> --tcp\n"
         )
         print("  --version, -v\t\t\t Display version information and exit")
         print("  --help, -h\t\t\t Display this help text and exit\n")
