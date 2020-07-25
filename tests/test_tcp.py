8984cc1e3fc8f81e9970c5c2df03fc83d6bbea91
Mark W. Hunter <marcus.w.hunter@gmail.com>
2020-07-25 12:06:52 -0400

    Linter changes

--- tests/test_tcp.py
+++ tests/test_tcp.py
@@ -1,12 +1,14 @@
+"""Test cases for dns_query_tcp function."""
 import pytest
 from dnsck import dnsck_query_tcp
 
 
 def test_tcp_query():
-    exit_status = dnsck_query_tcp("8.8.8.8", "google.com", "AAAA", 1)
-    assert exit_status == 0
+    """Tests for successful DNS query using UDP."""
+    assert dnsck_query_tcp("8.8.8.8", "google.com", "AAAA", 1) == 0
 
 
 def test_tcp_unknown_rec_type():
+    """Unknown record types should raise an exception and exit."""
     with pytest.raises(SystemExit):
-        assert dnsck_query_tcp("8.8.8.8", "google.com", "ABC")
+        assert dnsck_query_tcp("8.8.8.8", "google.com", "abc", 1)
--- tests/test_udp.py
+++ tests/test_udp.py
@@ -1,12 +1,14 @@
+"""Test cases for dns_query_udp function."""
 import pytest
 from dnsck import dnsck_query_udp
 
 
 def test_udp_query():
-    exit_status = dnsck_query_udp("8.8.8.8", "google.com", "a", 1)
-    assert exit_status == 0
+    """Tests for successful DNS query using UDP."""
+    assert dnsck_query_udp("8.8.8.8", "google.com", "a", 1) == 0
 
 
 def test_udp_unknown_rec_type():
+    """Unknown record types should raise an exception and exit."""
     with pytest.raises(SystemExit):
-        assert dnsck_query_udp("8.8.8.8", "google.com", "XYZ")
+        assert dnsck_query_udp("8.8.8.8", "google.com", "XYZ", 1)
