39cebdbf0acb501eeb2b56bbc8c92fd83b295bd4
Mark W. Hunter <marcus.w.hunter@gmail.com>
2020-07-26 13:46:20 -0400

    Update cli test cases to work with argparse exit codes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -33,26 +33,26 @@ def test_cli_version_alt():
 def test_cli_no_param():
     """Tests command-line input with no parameters."""
     cmd = ["python", "dnsck/dnsck.py"]
-    process = subprocess.run(cmd, shell=False, check=True)
-    assert process.returncode == 0
+    process = subprocess.run(cmd, shell=False, check=False)
+    assert process.returncode == 2
 
 
 def test_cli_extra_param():
     """Tests command-line input with extra parameter."""
     cmd = ["python", "dnsck/dnsck.py", "-s", "8.8.8.8", "google.com", "-x"]
-    process = subprocess.run(cmd, shell=False, check=True)
-    assert process.returncode == 0
+    process = subprocess.run(cmd, shell=False, check=False)
+    assert process.returncode == 2
 
 
 def test_cli_extra_param_tcp():
     """Tests command-line input with extra parameter and --tcp specified."""
     cmd = ["python", "dnsck/dnsck.py", "-s", "8.8.8.8", "google.com", "--tcp", "-y"]
-    process = subprocess.run(cmd, shell=False, check=True)
-    assert process.returncode == 0
+    process = subprocess.run(cmd, shell=False, check=False)
+    assert process.returncode == 2
 
 
 def test_cli_invalid_param():
     """Tests command-line input with invalid parameter."""
     cmd = ["python", "dnsck/dnsck.py", "-x", "8.8.8.8"]
     process = subprocess.run(cmd, shell=False, check=False)
-    assert process.returncode == 1
+    assert process.returncode == 2
