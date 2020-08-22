ec592b1e6a4976cc5d0a4226aa0c98aa32f07ba5
Mark W. Hunter <marcus.w.hunter@gmail.com>
2020-08-22 18:45:34 -0400

    Add test cases for IP address validation

---
+++ tests/test_ip.py
@@ -0,0 +1,22 @@
+"""Test cases for IP address validation."""
+from dnsck.dnsck import is_valid_ipv4_address, is_valid_ipv6_address
+
+
+def test_valid_ipv4():
+    """Tests valid IPv4 address."""
+    assert is_valid_ipv4_address("192.168.0.55") is True
+
+
+def test_invalid_ipv4():
+    """Tests invalid IPv4 address."""
+    assert is_valid_ipv4_address("192.168.0.256") is False
+
+
+def test_valid_ipv6():
+    """Tests valid IPv6 address."""
+    assert is_valid_ipv6_address("::1") is True
+
+
+def test_invalid_ipv6():
+    """Tests invalid IPv6 address."""
+    assert is_valid_ipv6_address("::Z") is False
