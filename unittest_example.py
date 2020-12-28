b4e8755c985f02087b9f1757a57bd9458ffde1e4
Mark W. Hunter <marcus.w.hunter@gmail.com>
2020-12-28 18:48:34 -0500

    Update unit tests to use combined IP validation

--- unittest_example.py
+++ unittest_example.py
@@ -1,6 +1,6 @@
 """IP address validation unit test cases."""
 import unittest
-from dnsck.dnsck import is_valid_ipv4_address, is_valid_ipv6_address
+from dnsck.dnsck import is_valid_ip_address
 
 
 class TestIPAddressValidation(unittest.TestCase):
@@ -8,19 +8,19 @@ class TestIPAddressValidation(unittest.TestCase):
 
     def test_valid_ipv4(self):
         """Tests valid IPv4 address."""
-        self.assertEqual(is_valid_ipv4_address("192.168.0.55"), True)
+        self.assertEqual(is_valid_ip_address("192.168.0.55"), True)
 
     def test_invalid_ipv4(self):
         """Tests invalid IPv4 address."""
-        self.assertEqual(is_valid_ipv4_address("192.168.0.256"), False)
+        self.assertEqual(is_valid_ip_address("192.168.0.256"), False)
 
     def test_valid_ipv6(self):
         """Tests valid IPv6 address."""
-        self.assertEqual(is_valid_ipv6_address("::1"), True)
+        self.assertEqual(is_valid_ip_address("::1"), True)
 
     def test_invalid_ipv6(self):
         """Tests invalid IPv6 address."""
-        self.assertEqual(is_valid_ipv6_address("::Z"), False)
+        self.assertEqual(is_valid_ip_address("::Z"), False)
 
 
 if __name__ == "__main__":
