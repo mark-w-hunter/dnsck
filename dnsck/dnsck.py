973d4efb8ec02a55bb147767f8f879bc957f2ebe
Mark W. Hunter <marcus.w.hunter@gmail.com>
2021-01-27 22:44:25 -0500

    Combine UDP and TCP query functions - don't repeat yourself

--- dnsck/dnsck.py
+++ dnsck/dnsck.py
@@ -27,7 +27,7 @@
 # SOFTWARE.
 
 
-"""This program performs automated DNS queries from command-line input."""
+"""Perform automated DNS queries from command-line input."""
 import sys
 import time
 import argparse
@@ -37,17 +37,18 @@ from typing import DefaultDict, List
 from dns import query, message, rcode, exception, rdatatype
 
 __author__ = "Mark W. Hunter"
-__version__ = "0.27"
+__version__ = "0.28"
 
 
-def dnsck_query_udp(dns_server: str, dns_query: str, record_type: str, iterations: int) -> int:
-    """Perform a UDP DNS query for a set number of iterations.
+def dnsck_query(dns_server: str, dns_query: str, record_type: str, iterations: int, tcp: bool = False) -> int:
+    """Perform a DNS query for a set number of iterations.
 
     Args:
         dns_server (str): IP address of server.
         dns_query (str): Query to search.
         record_type (str): Record type.
         iterations (int): Number of iterations.
+        tcp (bool): Use TCP for query.
 
     Returns:
         int: Number of errors.
@@ -73,7 +74,10 @@ def dnsck_query_udp(dns_server: str, dns_query: str, record_type: str, iteration
         for iteration in range(iterations):
             print(f"[Query {iteration + 1} of {iterations}]")
             try:
-                dns_response = query.udp(make_dns_query, dns_server, timeout=10)
+                if tcp:
+                    dns_response = query.tcp(make_dns_query, dns_server, timeout=10)
+                else:
+                    dns_response = query.udp(make_dns_query, dns_server, timeout=10)
                 if dns_response.answer:
                     for answer in dns_response.answer:
                         print(answer)
@@ -111,84 +115,6 @@ def dnsck_query_udp(dns_server: str, dns_query: str, record_type: str, iteration
     print(
         f"\nSummary: Performed {iteration_count} queries to server {dns_server}",
         f"for domain {dns_query} with record type {record_type.upper()}.",
-        f"\nResponse errors: {response_errors / iteration_count * 100:.2f}%",
-    )
-    print(f"Average response time: {sum(query_times) / len(query_times):.2f} ms\n")
-
-    return response_errors
-
-
-def dnsck_query_tcp(dns_server: str, dns_query: str, record_type: str, iterations: int) -> int:
-    """Perform a TCP DNS query for a set number of iterations.
-
-    Args:
-        dns_server (str): IP address of server.
-        dns_query (str): Query to search.
-        record_type (str): Record type.
-        iterations (int): Number of iterations.
-
-    Returns:
-        int: Number of errors.
-
-    """
-    result_code_dict: DefaultDict[str, int] = defaultdict(int)
-    query_times = []  # type: List[float]
-    record_number = 0  # type: int
-    response_errors = 0  # type: int
-    iteration_count = 0  # type: int
-
-    try:
-        make_dns_query = message.make_query(dns_query, record_type.upper())
-    except rdatatype.UnknownRdatatype:
-        print("Unknown record type, try again.")
-        sys.exit(1)
-    print(
-        f"Performing {iterations} TCP queries to server {dns_server} for domain name {dns_query}",
-        f"with record type {record_type.upper()}.\n",
-    )
-
-    try:
-        for iteration in range(iterations):
-            print(f"[Query {iteration + 1} of {iterations}]")
-            try:
-                dns_response = query.tcp(make_dns_query, dns_server, timeout=10)
-                if dns_response.answer:
-                    for answer in dns_response.answer:
-                        print(answer)
-                        record_number = len(answer)
-                else:
-                    print("No records returned.")
-                elapsed_time = dns_response.time * 1000  # type: float
-                if elapsed_time < 500:
-                    result_code = rcode.to_text(dns_response.rcode())  # type: str
-                    result_code_dict[result_code] += 1
-                    iteration_count += 1
-                else:
-                    result_code = "Degraded"
-                    result_code_dict[result_code] += 1
-                    iteration_count += 1
-                    response_errors += 1
-            except exception.Timeout:
-                print("Query timeout.")
-                result_code = "Timeout"
-                result_code_dict[result_code] += 1
-                elapsed_time = 10000
-                iteration_count += 1
-                response_errors += 1
-            time.sleep(1)
-            query_times.append(elapsed_time)
-            print(f"Records returned: {record_number}")
-            print(f"Response time: {elapsed_time:.2f} ms")
-            print(f"Response status: {result_code}\n")
-    except KeyboardInterrupt:
-        print("Program terminating...")
-
-    print("Response status breakdown:")
-    for query_rcode, count in result_code_dict.items():
-        print(f"{count} {query_rcode}")
-    print(
-        f"\nSummary: Performed {iteration_count} TCP queries to server {dns_server}",
-        f"for domain name {dns_query} with record type {record_type.upper()}.",
         f"\nResponse errors: {response_errors / iteration_count * 100:.2f}%",
     )
     print(f"Average response time: {sum(query_times) / len(query_times):.2f} ms\n")
@@ -246,9 +172,9 @@ def main():
         sys.exit(2)
 
     if args.tcp:
-        dnsck_query_tcp(args.server, args.domain, args.type, args.iter)
+        dnsck_query(args.server, args.domain, args.type, args.iter, True)
     else:
-        dnsck_query_udp(args.server, args.domain, args.type, args.iter)
+        dnsck_query(args.server, args.domain, args.type, args.iter)
 
 
 if __name__ == "__main__":
